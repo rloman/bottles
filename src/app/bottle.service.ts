69854ecfaee65e7dfeb25addf8a4507e737550d6
Raymond Loman <raymond@carpago.nl>
2018-08-08 13:27:38 +0200

    Add some imports to bottleservice to have rest enabled for later / now

--- src/app/bottle.service.ts
+++ src/app/bottle.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Bottle } from './bottle';
+import { Http, Headers } from '@angular/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,3 +10,9 @@ export class BottleService {
 
   constructor() { }
 }
+
+
+//
+
+// import { AuthenticationService } from './authentication.service';
+
