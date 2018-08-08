d88b82a1f23d712b363a6362bc04491b5861f8b4
Raymond Loman <raymond@carpago.nl>
2018-08-08 12:58:25 +0200

    Add bottle component (and consequences)

---
+++ src/app/bottle/bottle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bottle',
+  templateUrl: './bottle.component.html',
+  styleUrls: ['./bottle.component.css']
+})
+export class BottleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
