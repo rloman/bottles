df3c6a28d99033d6a86c5735f3def4d25b8f5429
Raymond Loman <raymond@carpago.nl>
2018-08-11 09:49:28 +0200

    Add the final REST user service using typicode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; // met of zonder src/client?
+import { BottleService } from 'src/app/bottle.service';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,19 @@ export class AppComponent {
 
   private url: string = "http://jsonplaceholder.typicode.com/users";
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private bottleService: BottleService) {
   }
 
   ngOnInit() {
     this.httpClient.get(this.url).subscribe((users) => {
-        console.log(users);
+        // console.log(users);
+    });
+
+    this.bottleService.get().subscribe(users => {
+      console.log("this is from my service");
+      console.log(users);
+      console.table(users);
+
     });
   }
 }
--- src/app/bottle.service.ts
+++ src/app/bottle.service.ts
@@ -2,18 +2,21 @@ import { Injectable } from '@angular/core';
 import { Bottle } from './bottle';
 import { Http, Headers } from '@angular/http';
 import { environment } from '../environments/environment';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BottleService {
 
-  private url: String = "http://jsonplaceholder.typicode.com/users";
+  private url: string = "http://jsonplaceholder.typicode.com/users";
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
-  
+  get() {
+    return this.httpClient.get<User[]>(this.url);
+  }
 }
 
 
